c9662d8331c7d2d641f5af3c9226d3c8c8177092
eeta <hwlim.me@gmail.com>
2021-01-11 03:09:23 +0900

    Add DI and invoke insert method to member controller

--- journal/src/main/java/com/mycompany/journal/member/controller/MemberController.java
+++ journal/src/main/java/com/mycompany/journal/member/controller/MemberController.java
@@ -1,13 +1,18 @@
 package com.mycompany.journal.member.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.mycompany.journal.member.mapper.MemberSQLMapper;
 import com.mycompany.journal.vo.MemberVo;
 
 @Controller
 @RequestMapping("/member/*")
 public class MemberController {
+	
+	@Autowired
+	private MemberSQLMapper memberSQLMapper;
 	
 	@RequestMapping("sign_in_page.do")
 	public String signInPage() {
@@ -23,6 +28,8 @@ public class MemberController {
 	
 	@RequestMapping("sign_up_process.do")
 	public String signUpProcess(MemberVo param) {
+		
+		memberSQLMapper.insert(param);
 		
 		return "member/sign_up_complete";
 	}
