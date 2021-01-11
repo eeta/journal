13fb59dd1dd58cc87f4e73c29212babb11e1103d
eeta <hwlim.me@gmail.com>
2021-01-11 12:31:49 +0900

    Add signInProcess method to member controller

--- journal/src/main/java/com/mycompany/journal/member/controller/MemberController.java
+++ journal/src/main/java/com/mycompany/journal/member/controller/MemberController.java
@@ -1,5 +1,7 @@
 package com.mycompany.journal.member.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,5 +34,22 @@ public class MemberController {
 		memberService.signUp(param);
 		
 		return "member/sign_up_complete_page";
+	}
+	
+	@RequestMapping("sign_in_process.do")
+	public String signInProcess(MemberVo param, HttpSession session) {
+		
+		MemberVo sessionUser = memberService.signIn(param);
+		
+		if (sessionUser != null) {
+			session.setAttribute("sessionUser", sessionUser);
+			
+			return "redirect:../content/main_page.do";
+			
+		} else {
+			
+			return "member/sign_in_fail_page";
+			
+		}
 	}
 }
