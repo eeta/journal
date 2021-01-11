b1959bfcc69af2405c8bdffeefa3bb094d6b8efc
eeta <hwlim.me@gmail.com>
2021-01-11 11:00:17 +0900

    Add member service impl with signUp method and DI

---
+++ journal/src/main/java/com/mycompany/journal/member/service/MemberServiceImpl.java
@@ -0,0 +1,20 @@
+package com.mycompany.journal.member.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mycompany.journal.member.mapper.MemberSQLMapper;
+import com.mycompany.journal.vo.MemberVo;
+
+@Service
+public class MemberServiceImpl {
+	
+	@Autowired
+	private MemberSQLMapper memberSQLMapper;
+	
+	public void signUp(MemberVo vo) {
+		
+		memberSQLMapper.insert(vo);
+		
+	}
+}
