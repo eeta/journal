a81f00aef0f1e8a38ebba3b5aaf5140e72894ace
eeta <hwlim.me@gmail.com>
2021-01-12 16:57:00 +0900

    Add DI and invoke insert method to content service impl

--- journal/src/main/java/com/mycompany/journal/content/service/ContentServiceImpl.java
+++ journal/src/main/java/com/mycompany/journal/content/service/ContentServiceImpl.java
@@ -1,13 +1,20 @@
 package com.mycompany.journal.content.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.mycompany.journal.content.mapper.ContentSQLMapper;
 import com.mycompany.journal.vo.ContentVo;
 
 @Service
 public class ContentServiceImpl {
 	
+	@Autowired
+	private ContentSQLMapper contentSQLMapper;
+	
 	public void writeContent(ContentVo vo) {
+		
+		contentSQLMapper.insert(vo);
 		
 	}
 }
