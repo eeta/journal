db58783fbe3d354b1b5c8af1b63fddef01a55aa9
eeta <hwlim.me@gmail.com>
2021-01-12 16:49:08 +0900

    Update writeContentProcess method to content controller for using member_no

--- journal/src/main/java/com/mycompany/journal/content/controller/ContentController.java
+++ journal/src/main/java/com/mycompany/journal/content/controller/ContentController.java
@@ -1,11 +1,14 @@
 package com.mycompany.journal.content.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.mycompany.journal.content.service.ContentServiceImpl;
 import com.mycompany.journal.vo.ContentVo;
+import com.mycompany.journal.vo.MemberVo;
 
 @Controller
 @RequestMapping("/content/*")
@@ -27,7 +30,13 @@ public class ContentController {
 	}
 	
 	@RequestMapping("write_content_process.do")
-	public String writeContentProcess(ContentVo param) {
+	public String writeContentProcess(HttpSession session, ContentVo param) {
+		
+		MemberVo sessionUser = (MemberVo)session.getAttribute("sessionUser");
+		
+		int member_no = sessionUser.getMember_no();
+		
+		param.setMember_no(member_no);
 		
 		contentService.writeContent(param);
 		
