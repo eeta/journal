c2ef4fbccc9e392e303d3a6be7bc62925beb59a7
eeta <hwlim.me@gmail.com>
2021-01-12 16:29:48 +0900

    Add content service impl

---
+++ journal/src/main/java/com/mycompany/journal/content/service/ContentServiceImpl.java
@@ -0,0 +1,13 @@
+package com.mycompany.journal.content.service;
+
+import org.springframework.stereotype.Service;
+
+import com.mycompany.journal.vo.ContentVo;
+
+@Service
+public class ContentServiceImpl {
+	
+	public void writeContent(ContentVo vo) {
+		
+	}
+}
