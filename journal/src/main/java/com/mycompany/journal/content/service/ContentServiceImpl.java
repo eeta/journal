6a4c935bc59b28b2f989c6db573500e133fae6db
eeta <hwlim.me@gmail.com>
2021-01-12 18:19:15 +0900

    Add getContentList method and DI to content service impl

--- journal/src/main/java/com/mycompany/journal/content/service/ContentServiceImpl.java
+++ journal/src/main/java/com/mycompany/journal/content/service/ContentServiceImpl.java
@@ -1,10 +1,15 @@
 package com.mycompany.journal.content.service;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.mycompany.journal.content.mapper.ContentSQLMapper;
+import com.mycompany.journal.member.mapper.MemberSQLMapper;
 import com.mycompany.journal.vo.ContentVo;
+import com.mycompany.journal.vo.MemberVo;
 
 @Service
 public class ContentServiceImpl {
@@ -12,9 +17,34 @@ public class ContentServiceImpl {
 	@Autowired
 	private ContentSQLMapper contentSQLMapper;
 	
+	@Autowired
+	private MemberSQLMapper memberSQLMapper;
+	
 	public void writeContent(ContentVo vo) {
 		
 		contentSQLMapper.insert(vo);
 		
 	}
+	
+	public ArrayList<HashMap<String, Object>> getContentList() {
+		
+		ArrayList<HashMap<String, Object>> resultList = new ArrayList<HashMap<String,Object>>();
+		
+		ArrayList<ContentVo> contentList = contentSQLMapper.selectAll();
+		
+		for (ContentVo contentVo : contentList) {
+			int member_no = contentVo.getMember_no();
+			
+			MemberVo memberVo = memberSQLMapper.selectByNo(member_no);
+			
+			HashMap<String, Object> map = new HashMap<String, Object>();
+			map.put("memberVo", memberVo);
+			map.put("contentVo", contentVo);
+			
+			resultList.add(map);
+		}
+		
+		return resultList;
+	}
+	
 }
