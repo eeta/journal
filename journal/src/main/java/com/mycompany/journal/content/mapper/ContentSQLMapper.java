b78712cc306f59e10d3e7db4771369afad4dc45a
eeta <hwlim.me@gmail.com>
2021-01-12 16:59:51 +0900

    Add selectAll to content mapper interface and content XML mapper

--- journal/src/main/java/com/mycompany/journal/content/mapper/ContentSQLMapper.java
+++ journal/src/main/java/com/mycompany/journal/content/mapper/ContentSQLMapper.java
@@ -1,9 +1,13 @@
 package com.mycompany.journal.content.mapper;
 
+import java.util.ArrayList;
+
 import com.mycompany.journal.vo.ContentVo;
 
 public interface ContentSQLMapper {
 	
 	public void insert(ContentVo vo);
+	
+	public ArrayList<ContentVo> selectAll();
 
 }
